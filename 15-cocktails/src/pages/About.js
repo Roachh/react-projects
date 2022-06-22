199746cb3682ea3b45e9ee04700df06e4ba046b7
Roachh <thgebarata@gmail.com>
2022-06-21 23:08:24 -0300

    15 projetos completos

--- 15-cocktails/src/pages/About.js
+++ 15-cocktails/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+        consequuntur quis animi! Quis velit voluptatem soluta tempore laborum
+        quia excepturi itaque ut ipsam sequi neque dignissimos consequuntur
+        harum facilis minima asperiores, consequatur ipsa vero repudiandae quod
+        ad rerum veritatis tempora.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
